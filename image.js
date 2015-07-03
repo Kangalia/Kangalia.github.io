7c4dd3e736ddcbbe7a81c2a8860ae0d21615c5c5
Isaac Kangalia <isaac.kangalia@savannahinformatics.com>
2015-07-03 10:20:46 +0300

    Removed styling and javascript files Added asset files Finished Chapter Three

---
+++ image.js
@@ -0,0 +1,7 @@
+/**
+ * Created by kangalia on 7/3/15.
+ */
+
+var canvas = document.getElementById("sample-image");
+var context = canvas.getContext("2d");
+context.strokeRect ( left, top, width, height );
--- javascripts/image.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by kangalia on 7/3/15.
- */
-
-var canvas = document.getElementById("sample-image");
-var context = canvas.getContext("2d")
-context.strokeRect ( left, top, width, height )
